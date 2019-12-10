0696dca8ea0d8945e8034c5bbd9e74b7b56f8712
Anton Kalcik <7405311+akalcik@users.noreply.github.com>
2019-12-10 16:41:42 +0100

    Fix typo in directory name

---
+++ Apis/NetCoreCalculatorApi/Controllers/DoubleController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+
+namespace CalculatorApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoubleController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public DoubleController(ILogger<DoubleController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/double/5
+        [HttpGet("{number}")]
+        [Authorize("calc:double")]
+        public JsonResult Get(int number)
+        {
+            _logger.LogInformation("Get double of {number}", number);
+
+            var result = new JObject
+            {
+                ["result"] = number * 2
+            };
+
+            return new JsonResult(result);
+        }
+    }
+}
---
+++ Apis/NetCoreCalculatorApi/Controllers/SquareController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+
+namespace CalculatorApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SquareController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public SquareController(ILogger<SquareController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/square/5
+        [HttpGet("{number}")]
+        [Authorize("calc:square")]
+        public ActionResult<string> Get(int number)
+        {
+            _logger.LogInformation("Get double of {number}", number);
+
+            var result = new JObject
+            {
+                ["result"] = number * number
+            };
+
+            return new JsonResult(result);
+        }
+    }
+}
